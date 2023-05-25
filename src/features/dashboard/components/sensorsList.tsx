a5df3b5a57c30764a75a091ae25e88a5b4a11e6e
rita-scaletech <rita.savaliya@scaletech.xyz>
2023-05-25 12:49:58 +0530

    reset commit and remove key from map

--- src/features/dashboard/components/sensorsList.tsx
+++ src/features/dashboard/components/sensorsList.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { sensorTableHeader } from '../constant/dashboard'
 import { PbBody, PbCell, PbHead, PbRow, PbTable } from 'shared/components/table';
+import GoogleMap from './mapContainer';
 
 
 interface ITableProps {
@@ -19,49 +20,49 @@ interface ITableProps {
 }
 const SensorsList = () => {
     const sensorsData = require('../JSON/sensorData.json')
-    // console.log("SensorsList ~ sensorsData:", sensorsData)
-
     const [sensorsList, setSensorsList] = useState<ITableProps[]>()
 
-    const handleSensorData = () => {
-        setSensorsList(sensorsData.data.sensors)
-    }
-
-
     useEffect(() => {
-        handleSensorData()
+        setSensorsList(sensorsData.data.sensors)
     }, [])
 
 
     return (
         <div className='sensors--list-container m--0-auto'>
-            <div>
-                <h3 className='text--center'>Area wise sensor details</h3>
-            </div>
-            <PbTable className='sensors--list'>
-                <PbHead>
-                    <PbRow>
-                        {sensorTableHeader.map(({ name }, index) =>
-                            <PbCell key={index}>{name}</PbCell>
-                        )}
-                    </PbRow>
-                </PbHead>
-                <PbBody>
-                    {sensorsList && sensorsList.map(({ sensor_name, sensor_type, location, severity, temperature, latitude, co, longitude, pm25 }, index: number) =>
-                        <PbRow key={index}>
-                            <PbCell>{sensor_name}</PbCell>
-                            <PbCell>{sensor_type}</PbCell>
-                            <PbCell>{location}</PbCell>
-                            <PbCell>{severity}</PbCell>
-                            <PbCell>{temperature}</PbCell>
-                            <PbCell>{latitude}</PbCell>
-                            <PbCell>{co}</PbCell>
-                            <PbCell>{longitude}</PbCell>
-                            <PbCell>{pm25}</PbCell>
+            <div id='haze'>
+                <h3 className='text--center mb--20'>Ahmadabad's Air Pollution : Area wise sensor details</h3>
+                <PbTable className='sensors--list'>
+                    <PbHead>
+                        <PbRow>
+                            {sensorTableHeader.map(({ name }, index) =>
+                                <PbCell className='table-title' key={index}>{name}</PbCell>
+                            )}
                         </PbRow>
-                    )}
-                </PbBody>
-            </PbTable>
+                    </PbHead>
+                    <PbBody>
+                        {sensorsList && sensorsList.map(({ sensor_name, sensor_type, location, severity, temperature, latitude, co, longitude, pm25 }, index: number) =>
+                            <PbRow className='table-row' key={index}>
+                                <PbCell>{sensor_name}</PbCell>
+                                <PbCell>{sensor_type}</PbCell>
+                                <PbCell>{location}</PbCell>
+                                <PbCell>
+                                    <p>{severity}</p>
+                                </PbCell>
+                                <PbCell>{temperature}</PbCell>
+                                <PbCell>{latitude}</PbCell>
+                                <PbCell>{co}</PbCell>
+                                <PbCell>{longitude}</PbCell>
+                                <PbCell>{pm25}</PbCell>
+                            </PbRow>
+                        )}
+                    </PbBody>
+                </PbTable>
+            </div>
+
+            <div id='aqi' className='mt--50'>
+                <h3 className='text--center mt--30 mb--20'>Ait Quality Index</h3>
+                <GoogleMap pollutionData={sensorsList} />
+            </div>
         </div>
     )
 }
